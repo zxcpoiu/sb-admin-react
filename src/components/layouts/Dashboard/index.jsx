e663d9add8d952570d65300c9a3e0d3def409340
Steve Steiner <ssteinerX@gmail.com>
2016-08-26 12:15:52 -0400

    Whitespace strip by saving in my editor.

--- src/components/layouts/Dashboard/index.jsx
+++ src/components/layouts/Dashboard/index.jsx
@@ -6,7 +6,7 @@ import $ from "jquery";
 import classNames from "classnames";
 
 var HomePage = React.createClass({
-    
+
   componentWillMount: function() {
     this.setState({Height: $(window).height()});
   },
@@ -16,13 +16,13 @@ var HomePage = React.createClass({
   },
 
   componentWillUnmount: function(){
-    
+
     $(window).unbind('resize',this.adjustResize);
 
   },
 
   getInitialState: function(){
-    
+
     return {
       uiElementsCollapsed: true,
       chartsElementsCollapsed: true,
@@ -35,7 +35,7 @@ var HomePage = React.createClass({
 
   toggleMenu: function(){
     if($(".navbar-collapse").hasClass('collapse')){
-      $(".navbar-collapse").removeClass('collapse');  
+      $(".navbar-collapse").removeClass('collapse');
     }
     else{
       $(".navbar-collapse").addClass('collapse');
@@ -59,22 +59,22 @@ var HomePage = React.createClass({
             </span>} fluid={true}  style={ {margin: 0} }>
             <ul className="nav navbar-top-links navbar-right">
               <Nav style={ {margin: 0} }>
-                
+
                 <NavDropdown title=<i className="fa fa-envelope fa-fw"></i> >
                   <MenuItem eventKey="1">
-                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
                     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
 
                   </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="2">
-                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
                     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
                   </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="3">
-                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div> 
-                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div> 
+                    <div> <strong>John Smith</strong> <span className="pull-right text-muted"> <em>Yesterday</em> </span> </div>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
                   </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="4">
@@ -84,40 +84,40 @@ var HomePage = React.createClass({
 
                 <NavDropdown title=<i className="fa fa-tasks fa-fw"></i> >
                   <MenuItem eventKey="1" style={ {width: 300} }>
-                    <div> 
-                      <p> <strong>Task 1</strong> <span className="pull-right text-muted">40% Complete</span> </p> 
+                    <div>
+                      <p> <strong>Task 1</strong> <span className="pull-right text-muted">40% Complete</span> </p>
                       <div>
-                        <ProgressBar active bsStyle="success" now={40} /> 
-                      </div> 
+                        <ProgressBar active bsStyle="success" now={40} />
+                      </div>
                     </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="2">
-                    <div> 
-                      <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p> 
+                    <div>
+                      <p> <strong>Task 2</strong> <span className="pull-right text-muted">20% Complete</span> </p>
                       <div>
                         <ProgressBar active bsStyle="info" now={20} />
-                      </div> 
-                    </div> 
-                  </MenuItem> 
+                      </div>
+                    </div>
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="3">
-                    <div> 
-                      <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p> 
+                    <div>
+                      <p> <strong>Task 3</strong> <span className="pull-right text-muted">60% Complete</span> </p>
                       <div>
                         <ProgressBar active bsStyle="warning" now={60} />
-                      </div> 
-                    </div> 
-                  </MenuItem> 
+                      </div>
+                    </div>
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="4">
-                    <div> 
-                      <p> <strong>Task 4</strong> <span className="pull-right text-muted">80% Complete</span> </p> 
+                    <div>
+                      <p> <strong>Task 4</strong> <span className="pull-right text-muted">80% Complete</span> </p>
                       <div>
                         <ProgressBar active bsStyle="danger" now={80} />
-                      </div> 
+                      </div>
                     </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="5">
                       <strong>See All Tasks</strong> <i className="fa fa-angle-right"></i>
@@ -127,23 +127,23 @@ var HomePage = React.createClass({
                 <NavDropdown title=<i className="fa fa-bell fa-fw"></i> >
                   <MenuItem eventKey="1" style={ {width: 300} }>
                     <div> <i className="fa fa-comment fa-fw"></i> New Comment <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="2">
                     <div> <i className="fa fa-twitter fa-fw"></i> 3 New Followers <span className="pull-right text-muted small">12 minutes ago</span> </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="3">
                     <div> <i className="fa fa-envelope fa-fw"></i> Message Sent <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="4">
                     <div> <i className="fa fa-tasks fa-fw"></i> New Task <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="5">
                     <div> <i className="fa fa-upload fa-fw"></i> Server Rebooted <span className="pull-right text-muted small">4 minutes ago</span> </div>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="6">
                     <strong>See All Alerts</strong> <i className="fa fa-angle-right"></i>
@@ -153,15 +153,15 @@ var HomePage = React.createClass({
                 <NavDropdown title=<i className="fa fa-user fa-fw"></i> >
                   <MenuItem eventKey="1">
                     <i className="fa fa-user fa-fw"></i> User Profile
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem eventKey="2">
                     <i className="fa fa-gear fa-fw"></i> Settings
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem eventKey="3">
                     <a href="http://www.strapui.com/" onClick={ () => { window.location='http://www.strapui.com/'; } }>
                       <i className="fa fa-eye fa-fw"></i> Premium React Themes
                     </a>
-                  </MenuItem> 
+                  </MenuItem>
                   <MenuItem divider />
                   <MenuItem eventKey="4">
                     <Link to="login">
@@ -171,10 +171,10 @@ var HomePage = React.createClass({
                 </NavDropdown>
 
               </Nav>
-            </ul> 
+            </ul>
             <div className="navbar-default sidebar" style={ { 'marginLeft': '-20px' } } role="navigation">
               <div className="sidebar-nav navbar-collapse">
-                
+
                 <ul className="nav in" id="side-menu">
 
                   <li className="sidebar-search">
@@ -187,11 +187,11 @@ var HomePage = React.createClass({
                       </span>
                     </div>
                   </li>
-              
+
                   <li>
                     <Link to="dashboard.home"><i className="fa fa-dashboard fa-fw"></i> &nbsp;Dashboard</Link>
                   </li>
-                        
+
                   <li className={classNames({'active': !this.state.chartsElementsCollapsed})}>
                     <a href="javascript:void(0)" onClick={ () => { this.setState({chartsElementsCollapsed: !this.state.chartsElementsCollapsed}); return false; } }>
                       <i className="fa fa-bar-chart-o fa-fw"></i> &nbsp;Charts<span className="fa arrow"></span>
@@ -205,30 +205,30 @@ var HomePage = React.createClass({
                       </li>
                     </ul>
                   </li>
-              
-                  <li> 
+
+                  <li>
                     <Link to="dashboard.tables"><i className="fa fa-table fa-fw"></i> &nbsp;Tables</Link>
-                  </li> 
-              
-                  <li> 
-                    <Link to="dashboard.forms"><i className="fa fa-edit fa-fw"></i> Forms</Link> 
                   </li>
-                    
-                  <li className={classNames({'active': !this.state.uiElementsCollapsed})}> 
-                    <a href="javascript:void(0)" onClick={ () => { this.setState({uiElementsCollapsed: !this.state.uiElementsCollapsed}); return false; } }><i className="fa fa-edit fa-fw"></i> UI Elements<span className="fa arrow"></span></a> 
+
+                  <li>
+                    <Link to="dashboard.forms"><i className="fa fa-edit fa-fw"></i> Forms</Link>
+                  </li>
+
+                  <li className={classNames({'active': !this.state.uiElementsCollapsed})}>
+                    <a href="javascript:void(0)" onClick={ () => { this.setState({uiElementsCollapsed: !this.state.uiElementsCollapsed}); return false; } }><i className="fa fa-edit fa-fw"></i> UI Elements<span className="fa arrow"></span></a>
 
                     <ul className={classNames({'nav nav-second-level': true, 'collapse': this.state.uiElementsCollapsed})}>
                       <li>
-                        <Link to="dashboard.panels-wells">Panels and Wells</Link> 
+                        <Link to="dashboard.panels-wells">Panels and Wells</Link>
                       </li>
                       <li>
-                        <Link to="dashboard.buttons">Buttons</Link> 
+                        <Link to="dashboard.buttons">Buttons</Link>
                       </li>
                       <li>
                         <Link to="dashboard.notifications">Notifications</Link>
                       </li>
                       <li>
-                        <Link to="dashboard.typography">Typography</Link> 
+                        <Link to="dashboard.typography">Typography</Link>
                       </li>
                       <li>
                         <Link to="dashboard.icons"> Icons</Link>
@@ -238,7 +238,7 @@ var HomePage = React.createClass({
                       </li>
                     </ul>
                   </li>
-                    
+
                   <li className={classNames({'active': !this.state.multiLevelDropdownCollapsed})}>
                     <a href="javascript:void(0)" onClick={ () => { this.setState({multiLevelDropdownCollapsed: !this.state.multiLevelDropdownCollapsed}); return false; } }>
                       <i className="fa fa-sitemap fa-fw"></i>&nbsp;Multi-Level Dropdown<span className="fa arrow"></span>
@@ -270,12 +270,12 @@ var HomePage = React.createClass({
                         </ul>
                       </li>
                     </ul>
-                  </li> 
-                    
+                  </li>
+
                   <li className={classNames({'active': !this.state.samplePagesCollapsed})}>
                     <a href="javascript:void(0)" onClick={ () => { this.setState({samplePagesCollapsed: !this.state.samplePagesCollapsed}); return false; } }>
                       <i className="fa fa-files-o fa-fw"></i>&nbsp;Sample Pages<span className="fa arrow"></span>
-                    </a> 
+                    </a>
                     <ul className={classNames({'nav nav-second-level': true, 'collapse': this.state.samplePagesCollapsed})}>
                       <li>
                         <Link to="dashboard.blank">Blank Page</Link>
@@ -285,7 +285,7 @@ var HomePage = React.createClass({
                       </li>
                     </ul>
                   </li>
-                    
+
                   <li>
                     <a href="http://www.strapui.com/">Premium React Themes</a>
                   </li>
@@ -296,21 +296,17 @@ var HomePage = React.createClass({
             </div>
 
           </Navbar>
-
           <div id="page-wrapper" className="page-wrapper" ref="pageWrapper" style={{minHeight: this.state.Height}}>
-            <RouteHandler {...this.props} />
-          </div>
-
+                 <RouteHandler {...this.props} />
+               </div>
         </div>
-
     );
   },
-
   statics: {
     fetchData: function(params) {
       }
   }
-  
+
 });
 
 export default HomePage;
