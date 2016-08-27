e97ebaca43823e1726230ccbde1da8fefcc922d2
Steve Steiner <ssteinerX@gmail.com>
2016-08-27 09:01:21 -0400

    Upgrade packages, mostly in devDependencies

--- test/main.js
+++ test/main.js
@@ -5,6 +5,12 @@
 // Babel Polyfill
 import 'babel-core/polyfill';
 
+/*
+August 27, 2016: ssteinerX
+
+Unfortunately, all the supporting files are missing.
+ repository, no idea what they
+should contain
 import './common/test_request';
 import './common/test_session';
 
@@ -13,3 +19,4 @@ import './routers/test_logged_out';
 
 import './pages/test_home_page';
 import './pages/test_landing_page';
+ */
